94198c4d3009da36416ce3ad2c105fc62dae753e
Alexey Ses <ses.alexey@gmail.com>
2021-12-09 20:52:07 +0100

    add basic e2e test

--- apps/test-nxapp-e2e/src/integration/app.spec.ts
+++ apps/test-nxapp-e2e/src/integration/app.spec.ts
@@ -10,4 +10,8 @@ describe('test-nxapp', () => {
     // Function helper example, see `../support/app.po.ts` file
     getGreeting().contains('Welcome to test-nxapp!');
   });
+
+  it('should have \'toggle popup\' button', () => {
+    cy.get('[data-cy=button-toggle-popup]').should('exist');
+  });
 });
