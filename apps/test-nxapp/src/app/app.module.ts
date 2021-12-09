3a5565c343f4c1d9167137ef6a2f95efbd250e17
Alexey Ses <ses.alexey@gmail.com>
2021-12-09 20:51:50 +0100

    add basic elements with animation

--- apps/test-nxapp/src/app/app.module.ts
+++ apps/test-nxapp/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { SomeTestComponent } from './some-test/some-test.component';
 
 @NgModule({
   declarations: [AppComponent, SomeTestComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- apps/test-nxapp/src/app/some-test/some-test.component.ts
+++ apps/test-nxapp/src/app/some-test/some-test.component.ts
@@ -1,12 +1,30 @@
+import { trigger, state, style, transition, animate } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'test-nxworkspace-some-test',
   templateUrl: './some-test.component.html',
-  styleUrls: ['./some-test.component.scss']
+  styleUrls: ['./some-test.component.scss'],
+  animations: [
+    trigger('popOverState', [
+      state('show', style({
+        opacity: 1
+      })),
+      state('hide', style({
+        opacity: 0
+      })),
+      transition('show => hide', animate('600ms ease-out')),
+      transition('hide => show', animate('1000ms ease-in'))
+    ])
+  ]
 })
 export class SomeTestComponent implements OnInit {
   someText = 'some-test works!';
+  showPopup = false;
+
+  get popupStateName() {
+    return this.showPopup ? 'show' : 'hide'
+  }
 
   ngOnInit(): void {
     console.log(`SomeTestComponent ngOnInit()`);
@@ -16,6 +34,10 @@ export class SomeTestComponent implements OnInit {
     console.log(`SomeTestComponent updateText()`);
 
     this.someText = 'text updated';
+  }
+
+  togglePopup() {
+    this.showPopup = !this.showPopup;
   }
 
 }
