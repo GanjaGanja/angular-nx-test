52e56c243be9e5c73b7c76466945a42dd508d4e2
Alexey Ses <ses.alexey@gmail.com>
2021-12-14 17:14:36 +0100

    improve template, add form component and e2e test

--- apps/test-nxapp-e2e/src/integration/app.spec.ts
+++ apps/test-nxapp-e2e/src/integration/app.spec.ts
@@ -3,8 +3,10 @@ import { getGreeting } from '../support/app.po';
 describe('test-nxapp', () => {
   beforeEach(() => {
     cy.visit('/');
+
     cy.get('[data-cy=button-toggle-popup]').as('buttonElement');
     cy.get('[data-cy=text-popup]').as('popupElement');
+    cy.get('[data-cy=form]').as('form');
   });
 
   it('should display welcome message', () => {
@@ -24,5 +26,20 @@ describe('test-nxapp', () => {
 
     cy.get('@buttonElement').click();
     cy.get('@popupElement').should('not.be.visible');
+  });
+
+  it('should send form on submit', () => {
+    cy.intercept('POST', '/api/new-user').as('postNewUser');
+
+    cy.get('@form').find('input[name=username]').type('TestUsername');
+    cy.get('@form').find('input[name=email]').type('test@e.mail');
+    cy.get('@form').submit();
+
+    cy.wait('@postNewUser').then(({ request }) => {
+      // not sure if it's correct to test it like that
+      expect(request.body).to.eq('username=TestUsername&email=test%40e.mail');
+    });
+
+    cy.visit('/');
   });
 });
